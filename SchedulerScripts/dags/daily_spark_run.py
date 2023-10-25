0960a8766c891f80dea6054aee9b8fc4858de3ab
Sai Thiha Zaw <sai.t.zaw@frontiir.net>
2023-10-25 22:45:16 +0630

    update the start date

--- SchedulerScripts/dags/daily_spark_run.py
+++ SchedulerScripts/dags/daily_spark_run.py
@@ -4,11 +4,10 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 
-
 # Default Dag declartion
 default_args = {
     "owner": "STHZ",
-    "start_date": pendulum.datetime(2023, 10, 23, tz="Asia/Rangoon"),
+    "start_date": pendulum.datetime(2023, 10, 24, tz="Asia/Rangoon"),
     "depends_on_past": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
@@ -16,7 +15,7 @@ default_args = {
 
 # Dags definition
 with DAG (
-    "spark_ELT_pipeline",
+    "pyspark_ELT_pipeline",
     default_args = default_args,
     schedule="@daily",
     tags = ['spark_ELT_pipeline']
